65f1005ac2a34e37ff0794d59745108f328ba02e
Jean-Pierre-P <je.portino@duocuc.cl>
2026-05-11 19:19:31 -0400

    sad

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // 1. Redirige las peticiones de productos al BFF
-      // Se activa cuando en el código usas '/api/bff/...'
       '/api/bff': {
-        target: 'http://localhost:8082', // Puerto donde corre tu greenbite-bff
+        target: 'http://localhost:8082', // Tu BFF
         changeOrigin: true,
-        secure: false,
-        // Mantiene la ruta completa al enviarla al backend de Java
         rewrite: (path) => path.replace(/^\/api\/bff/, '/api/bff')
       },
-      // 2. Redirige el Login, Registro y Usuarios al microservicio de Auth
-      // Se activa cuando en el código usas '/api/auth/...' o '/api/usuarios/...'
       '/api': {
-        target: 'http://localhost:8080', // Puerto por defecto de Spring Boot (Login/Auth)
-        changeOrigin: true,
-        secure: false
+        target: 'http://localhost:8080', // Microservicio de Login/Auth
+        changeOrigin: true
       }
     }
   }
